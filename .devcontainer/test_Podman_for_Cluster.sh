6b55d2197d719a819d3aad9c284c353e5b7e5113
deltadedirac <deltadedirac@gmail.com>
2023-01-10 23:25:46 +0100

    modifications to Dockerfile and Podmanfile to automate the tests

--- .devcontainer/test_Podman_for_Cluster.sh
+++ .devcontainer/test_Podman_for_Cluster.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-podman build -t explicit_disentanglement_proteins -f Podmanfile
-podman run --name gp_cpab_vitae_podman_test -it -v         explicit_disentanglement_proteins /bin/bash
+echo "The script for creating the container system must be executed inside .devcontainer folder present in the repo"
+cd ../../ && pwd
+#git clone --recursive https://github.com/deltadedirac/Explicit_Disentanglement_Molecules.git
+$1 build -f ./Explicit_Disentanglement_Molecules/.devcontainer/Podmanfile -t podman_disentanglement_proteins .
+$1 run --name gp_cpab_vitae_podman_test -it -v $(pwd)/Explicit_Disentanglement_Molecules/:/workspace/ podman_disentanglement_proteins /bin/bash
